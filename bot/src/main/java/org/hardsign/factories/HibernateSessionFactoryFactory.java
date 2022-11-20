90bd2276c3331ef520a3fcedf13264dfdc438c6b
lunev.d <lunev.d@kontur.ru>
2022-11-20 23:07:15 +0500

    Start debugging with console

--- bot/src/main/java/org/hardsign/factories/HibernateSessionFactoryFactory.java
+++ bot/src/main/java/org/hardsign/factories/HibernateSessionFactoryFactory.java
@@ -11,6 +11,8 @@ import org.jetbrains.annotations.NotNull;
 public class HibernateSessionFactoryFactory {
     public static SessionFactory create(BotDatabaseSettings settings) throws HibernateException {
         System.out.println("Start debugging with prints...");
+        System.out.println("Driver class name: " + org.postgresql.Driver.class.getName());
+
         var configuration = new Configuration()
                 .setProperty("hibernate.connection.driver_class", org.postgresql.Driver.class.getName())
                 .setProperty("hibernate.connection.url", createConnectionString(settings))
@@ -22,7 +24,7 @@ public class HibernateSessionFactoryFactory {
                     .setProperty("hibernate.connection.username", settings.getUser())
                     .setProperty("hibernate.connection.password", settings.getPassword());
 
-        System.out.println("configuration builded");
+        System.out.println("configuration build");
 
         var builder = new StandardServiceRegistryBuilder()
                 .applySettings(configuration.getProperties());
@@ -35,6 +37,7 @@ public class HibernateSessionFactoryFactory {
     @NotNull
     private static String createConnectionString(BotDatabaseSettings settings) {
         var databaseUrl = settings.getDatabaseUrl();
+        System.out.println(databaseUrl);
         // note (lunev.d): this means that we are launching on heroku
         if (databaseUrl != null)
             return databaseUrl;
